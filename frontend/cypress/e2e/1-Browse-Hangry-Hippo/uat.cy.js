6d518c4fc440aa6e595c10946ae91c0c8b0db3e8
platform-trial <robertjsteele@outlook.com>
2022-12-21 11:53:39 -0500

    add acceptance test

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/uat.cy.js
@@ -1,11 +1,17 @@
 describe('Shows Appeteasers', () => {
-  it('Connect to Dev Server', () => {
-    cy.visit('http://hangry-web-dev.us-east-1.elasticbeanstalk.com/');
-  });
-  it('selects Handhelds', () => {
-    //TODO: Add a test to check Handhelds
-  });
-  it('selects Appeteasers', () => {
-    //TODO: Add a test to check Appeteasers
-  });
+    it('Connect to Dev Server', () => {
+        cy.visit('http://hangry-web-dev.us-east-1.elasticbeanstalk.com/');
+    });
+    it('selects Handhelds', () => {
+        cy.contains('Handhelds').click();
+        cy.contains('Cheese Burger');
+        cy.contains('Fajita Tacos');
+    });
+    it('selects Appeteasers', () => {
+        cy.contains('Appeteasers').click();
+        cy.contains('Tater Tots');
+        cy.contains('Buffalo Wings');
+        cy.contains('Cheese Burger').should('not.exist');
+        cy.contains('Fajita Tacos').should('not.exist');
+    });
 });
